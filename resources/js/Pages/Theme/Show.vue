ae7c9f241cd408f96b347dda2560e702d3942e5a
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-04-10 01:27:46 +0500

    30

--- resources/js/Pages/Theme/Show.vue
+++ resources/js/Pages/Theme/Show.vue
@@ -65,9 +65,22 @@ export default {
                 theme_id: message.theme_id,
             }).then(res => {
                 message.is_not_solved_complaint = res.data.is_not_solved_complaint;
-
             });
-        }
+        },
+
+        storeImage(e) {
+            const file = e.target.files[0];
+            const formData = new FormData();
+            formData.append('image', file)
+
+            axios.post('/images', formData)
+                .then(res => {
+                    const image = `<img src="${res.data.url}" />`
+                    const editor = this.$refs.editor;
+                    const oldText = editor.innerHTML;
+                    editor.innerHTML = `${oldText} ${image}</br>`
+                })
+        },
     },
 
     layout: MainLayout
@@ -137,7 +150,17 @@ export default {
             <div class="mb-4">
                 <h3 class="text-xl mr-4">Добавить сообщение</h3>
             </div>
-            <div class="">
+            <div class="mb-4">
+                <div class="bg-gray-50 border border-gray-100 p-2 w-full">
+                    <div class="">
+                        <a @click="this.$refs.image.click()" class="block w-6" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                            </svg>
+                        </a>
+                        <input @change="storeImage" ref="image" type="file" hidden="true">
+                    </div>
+                </div>
                 <div ref="editor" class="w-full border border-gray-300 p-2 mb-4" contenteditable="true">
                 </div>
                 <div class="">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BranchController;
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SectionController;
@@ -48,6 +49,8 @@ Route::middleware('auth')->group(function () {
 
     Route::patch('/users/personal', [UserController::class, 'update'])
         ->name('users.personal');
+
+    Route::post('/images', [ImageController::class, 'store']);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
