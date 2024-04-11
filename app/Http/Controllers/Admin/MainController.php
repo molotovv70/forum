3e3f46861f6df984ebb4b04eac3a8338e242c28c
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-04-11 16:01:10 +0500

    35

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Branch;
+use App\Models\Message;
+use App\Models\Theme;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +14,24 @@ class MainController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Admin/Main/Index');
+        $stats = [
+            [
+                'title' => 'Ветки',
+                'count' => Branch::count()
+            ],
+            [
+                'title' => 'Темы',
+                'count' => Theme::count()
+            ],
+            [
+                'title' => 'Сообщения',
+                'count' => Message::count()
+            ],
+            [
+                'title' => 'Пользователи',
+                'count' => User::count()
+            ],
+        ];
+        return Inertia::render('Admin/Main/Index', ['stats' => $stats]);
     }
 }
--- resources/js/Pages/Admin/Main/Index.vue
+++ resources/js/Pages/Admin/Main/Index.vue
@@ -5,6 +5,10 @@ import AdminLayout from "@/Layouts/AdminLayout.vue";
 export default {
     name: "Index",
 
+    props: {
+        stats: Object
+    },
+
     components: {
         Link
     },
@@ -16,6 +20,14 @@ export default {
     <div class="">
         <div class="flex items-center mb-8">
             <h3 class="text-xl mr-4">Статистика</h3>
+        </div>
+        <div>
+            <div v-if="stats" class="flex justify-between text-center ">
+                <div v-for="stat in stats" class="">
+                    <h3>{{ stat.title }}</h3>
+                    <p>{{ stat.count }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
