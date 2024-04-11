963dd37f546c497596fecce8ab8131d9fe8b0b6c
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-04-11 14:47:58 +0500

    32

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Message\UpdateRequest;
 use App\Http\Resources\Message\MessageResource;
 use App\Models\Image;
 use App\Models\Message;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -41,7 +42,9 @@ class MessageController extends Controller
             function ($id) {
                 return Str::of($id)->replaceMatches('/@/', '')->value();
             }
-        );
+        )->filter(function ($id) {
+            return (User::where('id', $id)->exists());
+        });
 
         $imgIds = Str::of($data['content'])->matchAll('/img_id=[\d]+/')->unique()->transform(
             function ($id) {
